8fe113dedfe19aa0adf271f6ddd7d949a732a797
Thiago Santos <thilego418@gmail.com>
2024-04-25 16:17:10 -0300

    feat: :sparkles: Simples rotas

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ app.get('/', (req, res)=>{
     res.send("Olá! Servidor funcionando!");
 });
 
+app.get('/routing', (req, res) => {
+    res.send("Rota GET");
+});
+
+app.post('/routing', (req, res) => {
+    res.send("Rota POST");
+});
+
+app.put('/routing', (req, res) => {
+    res.send("Rota PUT");
+});
+
+app.patch('/routing', (req, res) => {
+    res.send("Rota PATCH");
+});
+
 app.listen(port, ()=>{
     console.log(`Servidor ouvindo na porta ${port}.`);
 });
